a7927ef579e6c948a7d53d1eb1b053490c29a2b7
eyal_borovsky <eyal.borovsky@beyondminds.ai>
2022-02-24 11:31:51 +0200

    finish controllers, provider, modules, middleware, exceptions, pipes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,7 @@
+/***
+ * A controller's purpose is to receive specific requests for the application
+ */
+
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
